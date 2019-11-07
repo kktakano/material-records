e84f6de5ff3955debcd434a2e6124e420cd95b7e
kktakano <kktakano03@gmail.com>
2019-11-07 14:04:51 +0900

    itemsテーブルのカラム作成

--- db/migrate/20191107042318_create_items.rb
+++ db/migrate/20191107042318_create_items.rb
@@ -1,8 +1,16 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
+      t.string :name, null: false
+      t.string :image
+      t.integer :price
+      t.decimal :cost, precision: 10, scale: 1, null: false
+      t.integer :stock, null: false
 
       t.timestamps
     end
+  end
+  def decimal
+    decimal.to_f
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_095338) do
+ActiveRecord::Schema.define(version: 2019_11_07_042318) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "image"
+    t.integer "price"
+    t.decimal "cost", precision: 10, scale: 1, null: false
+    t.integer "stock", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
