64690871d245c250c58a193f822e5f6f4681184c
kktakano <kktakano03@gmail.com>
2019-11-07 14:29:54 +0900

    material,use_materialモデル・カラム作成

---
+++ app/models/use_material.rb
@@ -0,0 +1,2 @@
+class UseMaterial < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_042318) do
+ActiveRecord::Schema.define(version: 2019_11_07_052007) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -20,6 +20,25 @@ ActiveRecord::Schema.define(version: 2019_11_07_042318) do
     t.integer "stock", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "materials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.integer "value", null: false
+    t.string "unit", null: false
+    t.string "image"
+    t.string "supplier"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "use_materials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "material_id"
+    t.decimal "price", precision: 10, scale: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["material_id"], name: "index_use_materials_on_material_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -36,4 +55,5 @@ ActiveRecord::Schema.define(version: 2019_11_07_042318) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "use_materials", "materials"
 end
