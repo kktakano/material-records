9132f5ecc6a9f62b4699b7fb29afbc766c07c44a
kktakano <kktakano03@gmail.com>
2019-11-07 14:44:50 +0900

    items_controllerへのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:edit, :update]
+  resources :items, only: [:new, :create, :edit, :update]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
