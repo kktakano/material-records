eaf9024395a5a1f5d511301e58fccd4d3dc681bc
kktakano <kktakano03@gmail.com>
2019-12-17 19:07:06 +0900

    adding_action_materials-controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:edit, :update]
   resources :items, only: [:new, :create, :edit, :update]
-  resources :material, only: [:new, :create, :edit, :update]
+  resources :material, only: [:index, :new, :create, :edit, :update]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
