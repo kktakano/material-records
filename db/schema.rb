0d5888d352845bfe3dbca3b3539fa0c7b61cb60e
kktakano <kktakano03@gmail.com>
2019-12-24 03:32:11 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_16_101645) do
+ActiveRecord::Schema.define(version: 2019_12_23_180313) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2019_12_16_101645) do
     t.string "name", null: false
     t.integer "price"
     t.decimal "cost", precision: 10, scale: 1, null: false
-    t.integer "stock", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
