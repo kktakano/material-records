0460c6be50b22b7ed86aa5b49b1ccf33cbb46a0d
kktakano <kktakano03@gmail.com>
2019-12-30 23:47:33 +0900

    using_material_price_generation
    使用する材料の量に対する金額を生成

--- app/assets/javascripts/cost_total_calculation.js
+++ app/assets/javascripts/cost_total_calculation.js
@@ -1,15 +1,13 @@
 $(function() {
-
-  function previewCost(){
+  // var total_cost = 0
     // 入力された値を元に材料の合計を計算
-    var input_value = $(".use-material-value").val()
-    // console.log(input_value)
-    var price_per_unit = $(".price-per-unit").val()
-    console.log(price_per_unit)
-  }
-
-  $(document).on('keyup', '.use-material-value', function(){
-    // console.log("発火")
-    previewCost();
+  $(document).on('keyup', '.use-material-value', function(total_cost){
+    // previewCost();
+    total_cost = 0 
+    var input_value = $(this).val()
+    var price_per_unit = $(this).attr("data_price-per-unit")
+    var use_material_cost = Math.round(input_value * price_per_unit * 10) / 10
+    total_cost = total_cost + use_material_cost
+    console.log(total_cost)
   })
 })
--- app/assets/javascripts/material-select.js
+++ app/assets/javascripts/material-select.js
@@ -5,12 +5,12 @@ $(function() {
                   <div class="form-group mb-2 col-sm-3">
                     <p class="form-control-plaintext" id="staticEmail2">${materialName}</p>
                   </div>
-                  <div class="form-group mx-sm-3 mb-2 price-per-unit" value="${pricePerUnit}">
+                  <div class="form-group mx-sm-3 mb-2 price-per-unit" data_price-per-unit="${pricePerUnit}">
                     <p class="mb-0 ">¥ ${pricePerUnit} / ${materialUnit}</p>
                   </div>
                   <div class="form-group mx-sm-3 mb-2">
                     <label for="inputPassword2" class="sr-only">Password</label>
-                    <input type="number" class="form-control use-material-value" placeholder="使用量を入力">
+                    <input type="number" class="form-control use-material-value" placeholder="使用量を入力" data_price-per-unit="${pricePerUnit}">
                     <p class="mb-0">${materialUnit}</P>
                     </div>
                   <button type="submit" class="btn btn-light mb-2 material_delete_button" value="${materialId}">削除</button>
