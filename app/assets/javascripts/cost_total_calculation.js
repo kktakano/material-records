8b1d228c9acc6afbb5673be2c87f61931bad6a67
kktakano <kktakano03@gmail.com>
2019-12-31 00:50:19 +0900

    Update cost_total_calculation.js
    使用材料の合計金額の計算

--- app/assets/javascripts/cost_total_calculation.js
+++ app/assets/javascripts/cost_total_calculation.js
@@ -1,13 +1,11 @@
 $(function() {
-  // var total_cost = 0
+  var total_cost = 0;
     // 入力された値を元に材料の合計を計算
-  $(document).on('keyup', '.use-material-value', function(total_cost){
-    // previewCost();
-    total_cost = 0 
-    var input_value = $(this).val()
-    var price_per_unit = $(this).attr("data_price-per-unit")
-    var use_material_cost = Math.round(input_value * price_per_unit * 10) / 10
-    total_cost = total_cost + use_material_cost
+  $(document).on('keyup', '.use-material-value', function(){
+    var input_value = $(this).val();
+    var price_per_unit = $(this).attr("data_price-per-unit");
+    var use_material_cost = Math.round(input_value * price_per_unit * 10) / 10;
+    total_cost += use_material_cost;
     console.log(total_cost)
   })
 })
