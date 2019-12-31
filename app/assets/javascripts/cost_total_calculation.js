d7abfad9e9064db113160e1ce30305662b278530
kktakano <kktakano03@gmail.com>
2019-12-31 18:00:51 +0900

    Update cost_total_calculation.js
    使用量の決定のボタンを押すと再入力ができないようにする

--- app/assets/javascripts/cost_total_calculation.js
+++ app/assets/javascripts/cost_total_calculation.js
@@ -9,7 +9,10 @@ $(function() {
     var price_per_unit = $(this).attr("data_price-per-unit");
     var use_material_cost = Math.round(input_value * price_per_unit * 10) / 10;
     total_cost += use_material_cost;
-    console.log(total_cost)
+    // 使用材料の合計金額をフォームに表示
     $(".total-cost-form").val(total_cost)
+    // 決定ボタンを押すと入力ができないようにする
+    adjacent_class.children(".use-material-value").attr("readonly",true)
+    
   })
 })
