315613b87ee2997cfb41b8f2be4701f620d8658d
kktakano <kktakano03@gmail.com>
2020-01-01 18:30:03 +0900

    Update material-select.js

--- app/assets/javascripts/material-select.js
+++ app/assets/javascripts/material-select.js
@@ -1,5 +1,5 @@
 $(function() {
-
+  // 商品登録ボタンの上に使用材料を追加表示
   function addMaterialHtml(materialId,materialName,pricePerUnit,materialUnit){
     var html = `<form class="form-inline mx-auto add-material" style="max-width: 60%;">
                   <div class="form-group mb-2 col-sm-3">
@@ -18,24 +18,24 @@ $(function() {
                 </form>`;
     $(".add-materials").append(html);
   }
-
+  // 材料の選択ボタンを押すと材料のデータをhtmlへ送信
   $(document).on('click', '.select_button', function(){
-    // console.log("発火")
     const materialId = $(this).attr("data_material_id")
-    // console.log(materialId)
     const materialName = $(this).attr("data_material_name")
     var materialPrice = $(this).attr("data_material_price")
     var materialValue = $(this).attr("data_material_value")
     // 単位あたりの金額を四捨五入で計算
     const pricePerUnit = Math.round(materialPrice / materialValue * 10) / 10
     const materialUnit = $(this).attr("data_material_unit")
+    // 選択した材料の表示を削除
     $(this)
     .parents(".material")
     .remove();
+    // 材料の情報を引数で渡す
     addMaterialHtml(materialId,materialName,pricePerUnit,materialUnit)
 
   })
-
+  // 生成した使用材料の表示にある削除ボタンを押すと表示を削除
   $(document).on('click','.material_delete_button', function(){
     $(this)
     .parents(".add-material")
