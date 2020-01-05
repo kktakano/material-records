ee58b145c52856711fd2a9669ee42dfb2695f2a6
kktakano <kktakano03@gmail.com>
2020-01-05 16:50:37 +0900

    Update item.rb
    itemのpriceにバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,14 @@ class Item < ApplicationRecord
   has_many :use_materials, dependent: :destroy
   has_one_attached :image
   accepts_nested_attributes_for :use_materials, allow_destroy: true
-  
+
   # nameのバリデーション
   validates :name,
     presence: true                         # 存在している事を検証
   # costのバリデーション
   validates :cost,
     presence: true                         # 存在している事を検証
+  # priceのバリデーション
+  validates :price,
+  length: { maximum: 9 }                  # 0.1〜99999999.9までの入力を許可
 end
