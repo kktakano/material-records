90a0ba263873f4353f09ba413820fe0683d5a446
kktakano <kktakano03@gmail.com>
2020-01-05 16:53:52 +0900

    Update use_material.rb
    use_materialにバリデーションの追加

--- app/models/use_material.rb
+++ app/models/use_material.rb
@@ -1,4 +1,11 @@
 class UseMaterial < ApplicationRecord
   belongs_to :item, optional: true
   belongs_to :material
+
+  # priceのバリデーション
+  validates :price,
+  presence: true                         # 存在している事を検証
+  # valueのバリデーション
+  validates :value,
+  presence: true                         # 存在している事を検証
 end
