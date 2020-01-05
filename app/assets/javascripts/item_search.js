d30d9d4d0884d566b1b7daa2eed9f53d5b1cbbba
kktakano <kktakano03@gmail.com>
2020-01-05 19:24:41 +0900

    Update item_search.js
    htmlの生成

--- app/assets/javascripts/item_search.js
+++ app/assets/javascripts/item_search.js
@@ -2,27 +2,30 @@ $(function() {
 
 
 
-  function appendMaterial(material) {
-    if (!material.image) {
-      var imageHtml = `<svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"/><text x="40%" y="50%" fill="#dee2e6" dy=".3em">NoImage</text></svg>`
+  function appendMaterial(item) {
+    if (!item.image) {
+      var imageHtml = `<svg class="card-img-top" width="100%" height="260" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"/><text x="40%" y="50%" fill="#dee2e6" dy=".3em">NoImage</text></svg>`
     } else {
-      var imageHtml = `<img class="bd-placeholder-img card-img-top", focusable="false", height="180", src="${material.image}", preserveaspectratio="xMidYMid slice", role= "img", style="object-fit: contain;" >`
+      var imageHtml = `<img class="card-img-top", focusable="false", height="260", src="${item.image}", preserveaspectratio="xMidYMid slice", role= "img", style="object-fit: contain;" >`
     }
   
-    var html = `<div class="card mx-auto mb-2 material" style='width: 18rem;'>
-                ${imageHtml}
-                <div class="card-body">
-                  <h5 class="card-title">${material.name}</h5>
-                </div>
-                <ul class="list-group list-group-flush">
-                  <li class="list-group-item">仕入れ値: ￥${material.price}</li>
-                  <li class="list-group-item">総数: ${material.value}</li>
-                  <li class="list-group-item">単位(set,cm): ${material.unit}</li>
-                </ul>
-                <div class="card-body">
-                  <button class="btn btn-sm btn-outline-secondary select_button" type="button" data_material_id="${material.id}" data_material_name="${material.name}" data_material_price="${material.price}" data_material_unit="${material.unit}" data_material_value="${material.value}">選択</button>
-                </div>
-             </div>`
+    var html = `<div class='col-md-4'>
+                  <div class='card mb-4 shadow-sm'>
+                    ${imageHtml}
+                    <div class="card-body">
+                      <p class='card-text'>name:${item.name}</p>
+                      <div class='d-flex justify-content-between align-items-center'>
+                        <div class='h6'>¥${item.price}</div>
+                      </div>
+                      <div class='d-flex justify-content-between align-items-center'>
+                        <div class='btn-group'>
+                          <button class='btn btn-sm btn-outline-secondary' type='button'>詳細</button>
+                        </div>
+                        <div class='h3 text-right'>cost: ¥${item.cost}</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>`
     $(".items-list").append(html)
     $(".pagination").empty()
   }
@@ -38,16 +41,16 @@ $(function() {
 
     $.ajax({
       type: 'get',
-      url: '/items/item-search',
+      url: '/items/item_search',
       data: {keyword: input},
       dataType: 'json'
     })
-    .done(function(materials){
-      console.log(materials)
+    .done(function(items){
+      console.log(items)
       $(".items-list").empty();
-      if (materials.length !== 0){
-        materials.forEach(function(material){
-          appendMaterial(material);
+      if (items.length !== 0){
+        items.forEach(function(item){
+          appendMaterial(item);
         });
       }
       else{
