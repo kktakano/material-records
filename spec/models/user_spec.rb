bdbd1d41cec7c7ad4f5594b7869e65522e8cf5dd
kktakano <kktakano03@gmail.com>
2020-01-08 17:05:09 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,6 @@ describe User do
 
     it "is invalid without a name" do
       urer = build(:user, nickname: nil)
-      user = User.new(name:"", email:"kkk@gmail.com", password:"00000000", password_confirmation:"00000000")
       user.valid?
       expect(user.errors[:name]).to include("translation missing: ja.activerecord.errors.models.user.attributes.name.blank")
     end
