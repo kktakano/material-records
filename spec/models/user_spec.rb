315f6bc8147b5485e535641081034343830c1421
kktakano <kktakano03@gmail.com>
2020-01-08 17:15:41 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,11 @@ describe User do
       user.valid?
       expect(user.errors[:name]).to include("translation missing: ja.activerecord.errors.models.user.attributes.name.blank")
     end
+
+    it "is invalid without a name" do
+      user = build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("translation missing: ja.activerecord.errors.models.user.attributes.email.blank")
+    end
   end
 end
