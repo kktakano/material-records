8ffd4ebfc2daacc03d2508156d9bf1c0673de85c
kktakano <kktakano03@gmail.com>
2020-01-13 00:59:06 +0900

    Update material_spec.rb

--- spec/models/material_spec.rb
+++ spec/models/material_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Material, type: :model do
   describe '#create' do
     context 'can save' do
-      
+      it 'is'
     end
     context 'can not save' do
       
