5e3940dda4af18cba7121bf68bad034266763084
kktakano <kktakano03@gmail.com>
2020-01-19 19:51:04 +0900

    Update use_material_spec.rb

--- spec/models/use_material_spec.rb
+++ spec/models/use_material_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe UseMaterial, type: :model do
   describe '#create' do
     context 'can save' do
-      
+      # price, item_id, material_id が存在すれば登録できる
+      it 'is valid with a price, user_id, material_id' do
+        expect(build(:use_material)).to be_valid
+      end
     end
     context 'can not save' do
       
