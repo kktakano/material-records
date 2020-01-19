779ae9f625d0f7a30f03935a0127381681f3c977
kktakano <kktakano03@gmail.com>
2020-01-19 20:30:29 +0900

    Update item_spec.rb

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
-Rspec.describe Item, type: :model do
+RSpec.describe Item, type: :model do
   describe '#create' do
     context 'can save' do
-      
+      # name, price, cost, user_id が存在すれば登録できる
+      it 'is valid with a name, price, cost, user_id' do
+        expect(build(:item)).to be_valid
+      end
+      # name, cost, user_id が存在すれば登録できる
+      it 'is valid with a name, cost, user_id' do
+        expect(build(:item, price: nil)).to be_valid
+      end
     end
     context 'can not save' do
       
