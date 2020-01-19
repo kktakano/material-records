a8aa3ef54a6cd11f8233cbea84f3911b02d67221
kktakano <kktakano03@gmail.com>
2020-01-19 20:23:55 +0900

    Update item_spec.rb

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+Rspec.describe Item, type: :model do
+  describe '#create' do
+    context 'can save' do
+      
+    end
+    context 'can not save' do
+      
+    end
+  end
+end
