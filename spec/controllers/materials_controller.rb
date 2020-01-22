4826798385c38bab0873649b32b8bd65771dcf96
kktakano <kktakano03@gmail.com>
2020-01-22 20:52:44 +0900

    Update materials_controller.rb

--- spec/controllers/materials_controller.rb
+++ spec/controllers/materials_controller.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 describe MaterialsController do
-  
+  # テスト中利用するインスタンスを定義
+  let(:user) { create(:user) }
+  # describe '#index' do
+  #   context 'log in' do
+  #     before do
+  #       login user
+  #       get :index, params: { material_id: material.id }
+  #     end
+  #   end
+
+  #   context 'not log in' do
+      
+  #   end
+  # end
+
+  describe 'GET #new' do
+    context 'log in' do
+      before do
+        login user
+      end
+      it "render the :new template" do
+        get :new
+        expect(response).to render_template :new
+      end    
+    end
+    context 'not log in' do
+      before do
+      end
+      it 'redirects to new_user_session_path' do
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 end
